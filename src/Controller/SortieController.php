6c0caef66f62d0fc4a8f9583fbd0041ba34961a7
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-09-02 14:04:05 +0200

    Version avec un formulaire qui s'affiche

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\RechercheSortieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +13,11 @@ final class SortieController extends AbstractController
     #[Route('', name: 'accueil')]
     public function accueil(): Response
     {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(RechercheSortieType::class, $sortie);
 
-        return $this->render('sortie/accueil.html.twig',);
+        return $this->render('sortie/accueil.html.twig',[
+            'sortieForm' => $sortieForm
+        ]);
     }
 }
---
+++ src/Form/RechercheSortieType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RechercheSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('nom', SearchType::class, [
+                'required' => false,
+                'label' => 'Le nom de la sortie contient :'
+            ])
+            ->add('dateHeureDebut',DateType::class,[
+                'property_path' => 'dateHeureDebut',
+                'required' => false,
+                'widget'=>'single_text',
+                'label' => 'Entre'
+            ])
+            ->add('dateHeureFin', DateType::class,[
+                'property_path' => 'dateHeureDebut',
+                'required' => false,
+                'widget'=>'single_text',
+                'label' => 'et'])
+            /**->add('etat', null, [
+                'required' => false,
+                'label' => 'sorties passées',
+            ]) **/
+            /**->add('organisateur', EntityType::class, [
+                'class' => Participant::class,
+                'choice_label' => 'id',
+            ])
+            ->add('participants', EntityType::class, [
+                'class' => Participant::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ]) **/
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
