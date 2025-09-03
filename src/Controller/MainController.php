585e720aface68e57879b5095376199f4a9cd9ca
kecaonet <trotin.kelan22190@gmail.com>
2025-09-03 14:06:17 +0200

    Main controller + profil / Update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,10 +9,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
-    public function home(): Response
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
     {
-        return $this->render("base.html.twig");
+        return $this->render("sortie/accueil.html.twig");
     }
+    #[Route('/Profil', name: 'profil')]
+    public function profil(): Response
+    {
+        return $this->render("profil/profil.html.twig");
+    }
+    #[Route('/ProfilUpdate', name: 'profilUpdate')]
+    public function profilUpdate(): Response
+    {
+        return $this->render("profil/profilUpdate.html.twig");
+    }
+
 
 
 }
